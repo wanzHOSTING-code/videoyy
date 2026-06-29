a9157fab2d21f5649d68302d4bb6bf8c137191b6
wanzHOSTING-code <jajanghermawan0812@gmail.com>
2026-06-29 22:00:52 +0700

    firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,10 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
+// Config kamu
 const firebaseConfig = {
-  apiKey: "AIzaSyCWNXxyQXW9w2IYjsGMRCjy0dHIMaHlykw",
+  apiKey: "AIzaSyCWNXxyQWw2IYjsGMRCjy0dHIMaHlykw",
   authDomain: "videoyy-6fbc1.firebaseapp.com",
   projectId: "videoyy-6fbc1",
   storageBucket: "videoyy-6fbc1.firebasestorage.app",
@@ -12,6 +12,11 @@ const firebaseConfig = {
   appId: "1:414930156563:web:ee75acc75e1471514c93e6"
 };
 
+// Init Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// Init Firestore
+const db = getFirestore(app);
+
+// export biar bisa dipakai file lain
+export { db };
