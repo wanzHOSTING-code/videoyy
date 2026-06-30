9ff8696c380d8b6d1420724f6fe9afe8c733e727
wanzHOSTING-code <jajanghermawan0812@gmail.com>
2026-06-30 08:16:45 +0700

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,10 +1,8 @@
-// Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-// Config kamu
 const firebaseConfig = {
-  apiKey: "AIzaSyCWNXxyQWw2IYjsGMRCjy0dHIMaHlykw",
+  apiKey: "AIzaSyCWNXxyQXW9w2IYjsGMRCjy0dHIMaHlykw",
   authDomain: "videoyy-6fbc1.firebaseapp.com",
   projectId: "videoyy-6fbc1",
   storageBucket: "videoyy-6fbc1.firebasestorage.app",
@@ -12,11 +10,6 @@ const firebaseConfig = {
   appId: "1:414930156563:web:ee75acc75e1471514c93e6"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
 
-// Init Firestore
-const db = getFirestore(app);
-
-// export biar bisa dipakai file lain
-export { db };
+export const db = getFirestore(app);
